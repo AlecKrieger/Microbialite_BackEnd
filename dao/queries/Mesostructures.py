5432dfe41d4e79f76011b3394d6731c89623a0cd
Lennart <buhl4063@stthomas.edu>
2023-12-13 21:45:07 -0600

    Structure + Naming
    Check the top of the file for convention

--- dao/queries/Mesostructures.py
+++ dao/queries/Mesostructures.py
@@ -1,4 +1,14 @@
-createMesoTable = """CREATE TABLE Mesostructure_Data(
+# Convention
+# create<name>Table
+# drop<name>Table
+# get<name>
+# get<name>ByID
+# insert<name>
+# update<name>
+# delete<name>
+
+# Mesostructure_Data
+createDataTable = """CREATE TABLE Mesostructure_Data(
 	mesostructureID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	mesostructureType VARCHAR(50),
     mesostructureTexture VARCHAR(50),
@@ -16,43 +26,7 @@ createMesoTable = """CREATE TABLE Mesostructure_Data(
     ON UPDATE CASCADE
     ON DELETE SET NULL
 );"""
-
-createSearchesMesoTable = """CREATE TABLE Searches_Meso(
-	userID INT UNSIGNED NOT NULL,
-	mesostructureID INT UNSIGNED NOT NULL,
-	PRIMARY KEY(userID, mesostructureID),
-	FOREIGN KEY (userID) REFERENCES User(userID)       
-	ON UPDATE CASCADE
-    ON DELETE CASCADE,
-    FOREIGN KEY (mesostructureID) REFERENCES Waypoint_Data(waypointID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);"""
-
-createAddMesoTable = """CREATE TABLE Add_Meso_Data(
-	analystID INT UNSIGNED NOT NULL,
-	mesostructureID INT UNSIGNED NOT NULL,
-	PRIMARY KEY(analystID, mesostructureID),
-	FOREIGN KEY (analystID) REFERENCES Analyst(analystID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE,
-    FOREIGN KEY (mesostructureID) REFERENCES Waypoint_Data(waypointID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);"""
-
-createPhotoTable = """CREATE TABLE Mesostructure_Photo(
-	mesostructureID INT UNSIGNED NOT NULL,
-	mesoPhotoID INT,
-	mesoPhoto VARCHAR(255),
-	PRIMARY KEY(mesostructureID, mesoPhotoID),
-    FOREIGN KEY (mesostructureID) REFERENCES Mesostructure_Data(mesostructureID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);"""
-
-# Mesostructure_Data
-dropData = "DROP TABLE IF EXISTS Mesostructure_Data;"
+dropDataTable = "DROP TABLE IF EXISTS Mesostructure_Data;"
 getData = "SELECT * FROM Mesostructure_Data;"
 getDataByID = "SELECT * FROM Mesostructure_Data WHERE mesostructureID = {mesostructureID};"
 insertData = """INSERT INTO Mesostructure_Data (
@@ -81,7 +55,6 @@ insertData = """INSERT INTO Mesostructure_Data (
     {wavelength}, 
     {amplitude}, 
     '{mesostructureGrains}');"""
-
 updateData = """UPDATE Mesostructure_Data SET 
     mesostructureType='{mesostructureType}', 
     mesostructureTexture='{mesostructureTexture}', 
@@ -97,10 +70,21 @@ updateData = """UPDATE Mesostructure_Data SET
     mesostructureGrains='{mesostructureGrains}' 
     WHERE 
     mesostructureID = {mesostructureID};"""
-
 deleteData = "DELETE FROM Mesostructure_Data WHERE mesostructureID = {mesostructureID};"
 
+
 # Searches_Meso
+createSearchesTable = """CREATE TABLE Searches_Meso(
+	userID INT UNSIGNED NOT NULL,
+	mesostructureID INT UNSIGNED NOT NULL,
+	PRIMARY KEY(userID, mesostructureID),
+	FOREIGN KEY (userID) REFERENCES User(userID)       
+	ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    FOREIGN KEY (mesostructureID) REFERENCES Waypoint_Data(waypointID)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);"""
 dropSearchesTable = "DROP TABLE IF EXISTS Searches_Meso;"
 getSearches = "SELECT * FROM Searches_Meso;"
 getSearchesByIDs = """SELECT * FROM Searches_Meso 
@@ -124,32 +108,52 @@ deleteSearches = """DELETE FROM Searches_Meso
                     AND 
                     mesostructureID = {mesostructureID};"""
 
-
 # Add_Meso_Data
-dropAddDataTable = "DROP TABLE IF EXISTS Add_Meso_Data;"
-getAddData = "SELECT * FROM Add_Meso_Data;"
-getAddDataByIDs = """SELECT * FROM Add_Meso_Data 
+createAddTable = """CREATE TABLE Add_Meso_Data(
+	analystID INT UNSIGNED NOT NULL,
+	mesostructureID INT UNSIGNED NOT NULL,
+	PRIMARY KEY(analystID, mesostructureID),
+	FOREIGN KEY (analystID) REFERENCES Analyst(analystID)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    FOREIGN KEY (mesostructureID) REFERENCES Waypoint_Data(waypointID)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);"""
+dropAddTable = "DROP TABLE IF EXISTS Add_Meso_Data;"
+getAdd = "SELECT * FROM Add_Meso_Data;"
+getAddByIDs = """SELECT * FROM Add_Meso_Data 
                      WHERE 
                      analystID = {analystID} 
                      AND 
                      mesostructureID = {mesostructureID};"""
-insertAddData = """INSERT INTO Add_Meso_Data (analystID, mesostructureID) 
+insertAdd = """INSERT INTO Add_Meso_Data (analystID, mesostructureID) 
                    VALUES 
                    ({analystID}, {mesostructureID});"""
-updateAddData = """UPDATE Add_Meso_Data 
+updateAdd = """UPDATE Add_Meso_Data 
                    SET 
                    analystID = {newAnalystID} 
                    WHERE 
                    analystID = {currentAnalystID} 
                    AND 
                    mesostructureID = {mesostructureID};"""
-deleteAddData = """DELETE FROM Add_Meso_Data 
+deleteAdd = """DELETE FROM Add_Meso_Data 
                    WHERE 
                    analystID = {analystID} 
                    AND 
                    mesostructureID = {mesostructureID};"""
 
+
 # Mesostructure_Photo
+createPhotoTable = """CREATE TABLE Mesostructure_Photo(
+	mesostructureID INT UNSIGNED NOT NULL,
+	mesoPhotoID INT,
+	mesoPhoto VARCHAR(255),
+	PRIMARY KEY(mesostructureID, mesoPhotoID),
+    FOREIGN KEY (mesostructureID) REFERENCES Mesostructure_Data(mesostructureID)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);"""
 dropPhotoTable = "DROP TABLE IF EXISTS Mesostructure_Photo;"
 getPhotos = "SELECT * FROM Mesostructure_Photo;"
 getPhotoByIDs = """SELECT * FROM Mesostructure_Photo 
